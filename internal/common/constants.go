dd6cee8d649248bea06b8b584614e3258f2f5e22
Kun-GitHub <hot_kun@hotmail.com>
2024-06-01 17:05:51 +0800

    //

--- internal/common/constants.go
+++ internal/common/constants.go
@@ -1,6 +1,6 @@
 // Copyright (c) [2024] K. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
-// Author: K.
+// Author: K. See：https://github.com/Kun-GitHub/RuoYi-Go
 // Email: hot_kun@hotmail.com or BusinessCallKun@gmail.com
 
 package common
