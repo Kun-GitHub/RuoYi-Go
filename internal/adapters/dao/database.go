56004a4698db8368e9f54121c07b6ec7b6e25c4a
Kun-GitHub <hot_kun@hotmail.com>
2024-07-22 16:32:46 +0800

    调整注释

--- internal/adapters/dao/database.go
+++ internal/adapters/dao/database.go
@@ -1,6 +1,6 @@
 // Copyright (c) [2024] K. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file. or see：https://github.com/Kun-GitHub/RuoYi-Go/blob/main/LICENSE
-// Author: K. See：https://github.com/Kun-GitHub/RuoYi-Go
+// Author: K. See：https://github.com/Kun-GitHub/RuoYi-Go or https://gitee.com/gitee_kun/RuoYi-Go
 // Email: hot_kun@hotmail.com or 867917691@qq.com
 
 package dao
