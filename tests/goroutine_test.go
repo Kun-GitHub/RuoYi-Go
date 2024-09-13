62bce54c3db9dd7f22970cae6c812d0c58307d6d
Kun-GitHub <hot_kun@hotmail.com>
2024-09-13 15:09:33 +0800

    fix(高并发): 模拟商城下单

---
+++ tests/goroutine_test.go
@@ -0,0 +1,59 @@
+// Copyright (c) [2024] K. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file. or see：https://github.com/Kun-GitHub/RuoYi-Go/blob/main/LICENSE
+// Author: K. See：https://github.com/Kun-GitHub/RuoYi-Go or https://gitee.com/gitee_kun/RuoYi-Go
+// Email: hot_kun@hotmail.com or 867917691@qq.com
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+// 商品库存结构体
+type Product struct {
+	mu    sync.Mutex // 互斥锁
+	stock int        // 库存
+	sales int        // 销售量
+}
+
+// 下单函数
+func (p *Product) PlaceOrder(n int) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if p.stock-n >= 0 {
+		p.stock -= n
+		p.sales += n
+		fmt.Printf("已下单成功，剩余库存：%d\n", p.stock)
+		return
+	}
+	fmt.Printf("下单失败，剩余库存：%d\n", p.stock)
+}
+
+func TestGoroutine(t *testing.T) {
+	// 初始化商品
+	product := &Product{stock: 50}
+
+	// 设置随机种子
+	rand.Seed(time.Now().UnixNano())
+
+	// 创建一个等待组，用于等待所有 goroutines 完成
+	var wg sync.WaitGroup
+
+	// 模拟1000个下单请求
+	for i := 0; i < 1000; i++ {
+		quantity := 1 // 假设每次下单只买一个商品
+		wg.Add(1)
+		go func(quantity int) {
+			defer wg.Done()
+			product.PlaceOrder(quantity)
+		}(quantity)
+	}
+
+	// 等待所有 goroutines 完成
+	wg.Wait()
+}
