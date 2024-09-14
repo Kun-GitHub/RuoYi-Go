a4dfd893016898023932773ef63db4e15ad9aef4
Kun-GitHub <hot_kun@hotmail.com>
2024-09-14 13:09:16 +0800

    fix(高并发): 模拟高并发下单多个商品
    如果代码用defer释放锁会出现死锁，在代码中，使用了 defer 来释放锁。defer 会在封闭函数返回时执行，而在循环中的 defer 会等到整个 placeOrder 函数执行完后才执行。也就是说，即使你在循环中已经锁定了某个产品，defer 只会在函数执行结束时解锁所有的产品，这就导致其他 goroutine 在等待这些锁时出现死锁。
    问题原因：
    defer 在每次循环中被调用，但不会立即释放锁，它是在当前函数返回时才释放锁。因为每个产品的锁是在循环中加的，直到整个循环结束，锁才会释放，导致多个 goroutine 卡在等待锁的状态，最终导致死锁。
    解决方法：
    应该在每次操作完成后立即解锁，而不是使用 defer。将 Unlock() 放在操作之后，确保每个商品的锁在使用后立即释放

--- tests/goroutine_2sales_1for_test.go
+++ tests/goroutine_2sales_1for_test.go
@@ -2,12 +2,15 @@
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file. or see：https://github.com/Kun-GitHub/RuoYi-Go/blob/main/LICENSE
 // Author: K. See：https://github.com/Kun-GitHub/RuoYi-Go or https://gitee.com/gitee_kun/RuoYi-Go
 // Email: hot_kun@hotmail.com or 867917691@qq.com
+
 package main
 
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"testing"
+	"time"
 )
 
 // 商品列表
@@ -17,33 +20,32 @@ var products = map[string]*Product{
 }
 
 // 下单函数
-func placeOrder(items map[string]int) string {
+func placeOrder(items map[string]int) {
 	// 逐个检查库存并扣减
 	for productID, quantity := range items {
 		product := products[productID]
-		product.mu.Lock()
-		defer func() {
-			fmt.Printf("解锁：%v\n", productID)
-			product.mu.Unlock()
-		}()
 		if product.stock-quantity >= 0 {
+			product.mu.Lock() // 锁定产品
 			product.stock -= quantity
 			product.sales += quantity
+			product.mu.Unlock() // 解锁产品
 			fmt.Printf("%v下单成功，剩余库存：%d\n", productID, product.stock)
 		} else {
 			fmt.Printf("%v下单失败，剩余库存：%d\n", productID, product.stock)
-			return "商品库存不足"
 		}
 	}
-	return "已下单成功"
 }
 
 func TestGoroutine2Sales1For(t *testing.T) {
-	var i = 0
+	// 设置随机种子
+	rand.Seed(time.Now().UnixNano())
+
+	// 创建一个等待组，用于等待所有 goroutines 完成
+	var wg sync.WaitGroup
+
 	// 模拟1000个下单请求
-	for {
-		i++
-		fmt.Printf("并发下单请求i：%d\n", i)
+	for i := 0; i < 1000; i++ {
+		fmt.Printf("并发下单i：%d\n", i)
 		items := make(map[string]int)
 		// 随机选择一个商品或两个商品一起下单
 		if rand.Intn(2) == 0 {
@@ -58,8 +60,14 @@ func TestGoroutine2Sales1For(t *testing.T) {
 			items["苹果"] = 1
 			items["栗子"] = 1
 		}
+
+		wg.Add(1)
 		go func(items map[string]int) {
+			defer wg.Done()
 			placeOrder(items)
 		}(items)
 	}
+
+	// 等待所有 goroutines 完成
+	wg.Wait()
 }
