f1f94fb097ab829922570c08362b3a8362638f86
Kun-GitHub <hot_kun@hotmail.com>
2024-10-22 16:34:32 +0800

    fix(优化): build 构建时自动复制配置文件到build目录

--- build/build.cmd
+++ build/build.cmd
@@ -9,6 +9,15 @@ if EXIST %SERVER%.exe del %SERVER%.exe
 cd ..
 SET ROOT=%CD%
 
+xcopy "%ROOT%\config" "%BLDIR%\config" /E /I /Q /Y
+REM 删除指定文件
+echo Removing app_config.go from build directory...
+if EXIST "%BLDIR%\config\app_config.go" (
+    del "%BLDIR%\config\app_config.go"
+) else (
+    echo File file_to_remove.txt does not exist.
+)
+
 echo Building %SERVER%
 cd %ROOT%\cmd\api
 go build -o %BLDIR%\%SERVER%.exe .
